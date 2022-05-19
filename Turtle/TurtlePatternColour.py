ed21983c94c8e22d64541811f14e6539ec88383a
Oliver Louis Deschamps <38859812+HalfFrenchOllie@users.noreply.github.com>
2022-05-19 14:20:07 +0100

    Add files via upload

--- Turtle/TurtlePatternColour.py
+++ Turtle/TurtlePatternColour.py
@@ -1,12 +1,15 @@
 import turtle
 import time
+import random
 
 x = 2
+colours = ["Red", "orange", "yellow", "green", "blue", "indigo", "violet"]
 
 while x < 10:
     tur = turtle.Turtle()
-    tur.color ('blue', 'black')
+    tur.color(random.choice(colours))
     tur.shape ('circle')
+    turtle.Screen().bgcolor("white")
     tur.speed (5)
     for i in range (x):
         tur.forward (100)
